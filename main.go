b8f812cce07675b0eb9023284465683ac0ed1c97
MagomedovMurad20 <magdev09@gmail.com>
2024-06-26 17:41:42 +0300

    добавил хендлер и эндпоинт

--- main.go
+++ main.go
@@ -7,25 +7,37 @@ import (
 )
 
 type product struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"product"`
-	Price  float64 `json:"price"`
+	ID      string  `json:"id"`
+	Title   string  `json:"title"`
+	Product string  `json:"product"`
+	Price   float64 `json:"price"`
 }
 
 var products = []product{
-	{ID: "1", Title: "kanfetka", Artist: "chiorio", Price: 52.52},
-	{ID: "2", Title: "moloko", Artist: "nemoloko", Price: 77.77},
-	{ID: "3", Title: "chokoladka", Artist: "alenka", Price: 66.66},
+	{ID: "1", Title: "kanfetka", Product: "chi orio", Price: 52.52},
+	{ID: "2", Title: "moloko", Product: "ne moloko", Price: 77.77},
+	{ID: "3", Title: "chokoladka", Product: "alenka", Price: 66.66},
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/product", getProducts)
-
+	router.GET("/products", getProducts)
+	router.POST("/products", postProducts)
 	router.Run("localhost:8080")
 }
 
 func getProducts(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, products)
+}
+
+func postProducts(c *gin.Context) {
+	var newProduct product
+
+	if err := c.BindJSON(&newProduct); err != nil {
+		return
+	}
+
+	products = append(products, newProduct)
+	c.IndentedJSON(http.StatusCreated, newProduct)
+
 }
